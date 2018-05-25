3e5c9df2f90239f76b0d7bc28770b3a00ce2e786
Luan Nguyen <nhatlang19@gmail.com>
2018-05-25 16:13:44 +0700

    add form create

--- app/src/main/java/com/tpgrade/tpgrade/Fragments/Home/CreateNewDialogFragment.java
+++ app/src/main/java/com/tpgrade/tpgrade/Fragments/Home/CreateNewDialogFragment.java
@@ -1,16 +1,11 @@
-package com.tpgrade.tpgrade.Fragments;
+package com.tpgrade.tpgrade.Fragments.Home;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.tpgrade.tpgrade.R;
 
--- app/src/main/java/com/tpgrade/tpgrade/HomeActivity.java
+++ app/src/main/java/com/tpgrade/tpgrade/HomeActivity.java
@@ -4,7 +4,6 @@ import android.app.DialogFragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,7 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.tpgrade.tpgrade.Fragments.CreateNewDialogFragment;
+import com.tpgrade.tpgrade.Fragments.Home.CreateNewDialogFragment;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
