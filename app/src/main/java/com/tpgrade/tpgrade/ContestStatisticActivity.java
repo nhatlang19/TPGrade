a28a7e729933fde2bb0e1837f2fa6171f98637c7
Luan Nguyen <nhatlang19@gmail.com>
2018-05-28 01:29:18 +0700

    aaa

--- app/src/main/java/com/tpgrade/tpgrade/ContestStatisticActivity.java
+++ app/src/main/java/com/tpgrade/tpgrade/ContestStatisticActivity.java
@@ -8,6 +8,6 @@ public class ContestStatisticActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_statistic);
+        setContentView(R.layout.activity_contest_statistic);
     }
 }
